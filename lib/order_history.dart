d30dd676715b9f966d56f6cf0ccc6b74b3a06fae
shakeel0232 <66824177+shakeel0232@users.noreply.github.com>
2021-06-26 17:49:25 -0700

    Create order_history.dart

---
+++ lib/order_history.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderHistory extends StatefulWidget {
+  const OrderHistory({Key? key}) : super(key: key);
+
+  @override
+  _OrderHistoryState createState() => _OrderHistoryState();
+}
+
+class _OrderHistoryState extends State<OrderHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
